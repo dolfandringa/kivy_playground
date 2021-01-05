947dead0d6c12591731fbfa7ec92897a95d897a0
Dolf Andringa <dolfandringa@gmail.com>
2021-01-05 14:44:15 +0800

    Added kivymd_app_icon

---
+++ kivymd_app_icon/test.py
@@ -0,0 +1,22 @@
+from kivy.uix.screenmanager import Screen
+
+from kivymd.app import MDApp
+from kivymd.uix.button import MDRectangleFlatButton
+
+
+class MainApp(MDApp):
+    def build(self):
+        self.title = "MyApp"
+        self.icon = "logo.png"
+        screen = Screen()
+        screen.add_widget(
+            MDRectangleFlatButton(
+                text="Hello, World",
+                pos_hint={"center_x": 0.5, "center_y": 0.5},
+            )
+        )
+
+        return screen
+
+
+MainApp().run()
